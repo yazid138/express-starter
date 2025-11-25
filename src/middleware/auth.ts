bdb24e851eed2cbd2fab5a563e649388cc2b124a
yazid138 <ma39isy@gmail.com>
2025-11-25 12:35:43 +0700

    commit

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,10 +1,10 @@
 import UnauthorizedException from "@/exception/UnauthorizedException";
 import passport from "passport";
 import { Request, Response, NextFunction } from "express";
-import UserResponse from "@/types/userResponse";
+import { User } from "@prisma/client";
 
 export default (req: Request, res: Response, next: NextFunction) =>
-  passport.authenticate("jwt", (err: boolean, user?: UserResponse) => {
+  passport.authenticate("jwt", (err: boolean, user?: User) => {
     try {
       if (err || !user) throw new UnauthorizedException();
       req.user = user;
